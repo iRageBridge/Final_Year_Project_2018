467998113de885c44c177091b3e540c8ca19081f
Simon Standbridge <simonstandbridge@gmail.com>
2018-02-21 16:23:53 +0000

    attempted CORS fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
-import * as functions from 'firebase-functions';
+//import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
+// // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+    const admin = require('firebase-admin');
+    const cors = require('cors')({origin: true});
+    admin.initializeApp(functions.config().firebase);
+
+  
+  exports.helloWorld = functions.https.onRequest((req, res) => {
+      cors(req, res, () => {
+          res.status(500).send("Firebase test");
+      });
+  });
+  
+  
