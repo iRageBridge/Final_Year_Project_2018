9b72b7575aa68573cee4fb2449172d415df48822
Simon Standbridge <simonstandbridge@gmail.com>
2018-04-05 14:16:10 +0100

    changed to backup database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,11 @@
 export const environment = {
   production: true,
   firebase: {
-      apiKey: "AIzaSyChEhv_139I3KimOvlxK-cLRDRb4rp4u5c",
-      authDomain: "irishpf-database.firebaseapp.com",
-      databaseURL: "https://irishpf-database.firebaseio.com",
-      projectId: "irishpf-database",
-      storageBucket: "irishpf-database.appspot.com",
-      messagingSenderId: "1027694931347"
+    apiKey: "AIzaSyDmEPL-H7jsha2-Ain9RtwTTSA2gTwBpRc",
+    authDomain: "fyp-2018-8dd70.firebaseapp.com",
+    databaseURL: "https://fyp-2018-8dd70.firebaseio.com",
+    projectId: "fyp-2018-8dd70",
+    storageBucket: "",
+    messagingSenderId: "294759606166"
   }
 };
